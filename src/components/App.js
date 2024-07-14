c6bb9c92f6796005ad58a2f9fb2bebdd28c81e83
Matthew Johnson <mtthw.jhnson@gmail.com>
2024-07-13 21:36:42 -0500

    working on message compiler.

--- src/components/App.js
+++ src/components/App.js
@@ -24,6 +24,10 @@ function App() {
   const [selectedCompany, setSelectedCompany] = useState(null);
   const [selectedTemplate, setSelectedTemplate] = useState(null);
 
+  console.log(selectedCompany);
+  console.log(selectedGuest);
+  console.log(selectedTemplate);
+
   //This function processes the time of day and returns Morning, Afternoon or Evening.
   const getTimeOfDay = () => {
     let timeOfDay = "";
@@ -54,44 +58,8 @@ function App() {
     setSelectedTemplate(selectedTemplate);
   };
 
-  //Compile our Message Together with the Data from the JSON Documents, using the ID numbers of the data objects in that document.
-  const messageGenerator = (dataSelected) => {
-    // We Need the Time of Day, and are generating that with this function
-    // Placing this function call here as it is not necessary to have Time in State until the message is generated.
-
-    //Define Data Objects from Selection into simpler syntax to make it more useable. -1 is for 0 index.
-    let guest = guests[dataSelected.guestId - 1];
-    let company = companies[dataSelected.companyId - 1];
-    let template = templates[dataSelected.templateId - 1];
-
-    //Generate message object template for future Use:
-    let dataObject = {
-      guest: {
-        firstName: guest.firstName,
-        lastName: guest.lastName,
-        roomNumber: guest.reservation.roomNumber,
-        checkIn: guest.reservation.startTimestamp,
-        checkOut: guest.reservation.endTimestamp,
-      },
-      company: {
-        name: company.company,
-        city: company.city,
-        timezone: company.timezone,
-      },
-      messageTemplate: {
-        timeofDay: timeofDay,
-        message: template.message,
-      },
-    };
-
-    //Create Message Variable with available data.
-    let messageOutput = `Good ${dataObject.messageTemplate.timeofDay} ${dataObject.guest.firstName} ${dataObject.guest.lastName}, 
-    and welcome to ${dataObject.company.name}!
-    Room ${guest.reservation.roomNumber} is ready for you. 
-    ${dataObject.messageTemplate.message}`;
-
-    //Set Message variable for rendering to DOM
-    setMessage(messageOutput);
+  const compileMessage = (message) => {
+    setMessage(message);
   };
 
   return (
@@ -112,7 +80,7 @@ function App() {
         selectedCompany={selectedCompany}
         selectedTemplate={selectedTemplate}
         timeOfDay={timeOfDay}
-        generateMessage={generateMessage}
+        compileMessage={compileMessage}
       />
       <MessageDisplay message={message} />
     </div>
--- src/components/MessageCompiler/MessageCompiler.jsx
+++ src/components/MessageCompiler/MessageCompiler.jsx
@@ -1,9 +1,46 @@
-function MessageCompiler() {
+import { useEffect } from "react";
 
-    return (
-        <>
-        </>
-    )
+function MessageCompiler({ selectedGuest, selectedCompany, selectedTemplate, timeOfDay, compileMessage }) {
+
+    const messageGenerator = (selectedGuest, selectedCompany, selectedTemplate, timeOfDay) => {
+
+        //Define Data Objects from Selection into simpler syntax to make it more useable. -1 is for 0 index.
+        let guest = selectedGuest
+        let company = selectedCompany
+        let template = selectedTemplate
+        let timeofDay = timeOfDay
+
+        //Generate message object template for future Use:
+        let dataObject = {
+            guest: {
+                firstName: guest.firstName,
+                lastName: guest.lastName,
+                roomNumber: guest.reservation.roomNumber,
+                checkIn: guest.reservation.startTimestamp,
+                checkOut: guest.reservation.endTimestamp,
+            },
+            company: {
+                name: company.company,
+                city: company.city,
+                timezone: company.timezone,
+            },
+            messageTemplate: {
+                timeofDay: timeofDay,
+                message: template.message,
+            },
+        };
+
+        // Create Message Variable with available data.
+        let messageOutput = `Good ${dataObject.messageTemplate.timeofDay} ${dataObject.guest.firstName} ${dataObject.guest.lastName}, 
+        and welcome to ${dataObject.company.name}!
+        Room ${guest.reservation.roomNumber} is ready for you. 
+        ${dataObject.messageTemplate.message}`;
+
+        //Set Message variable for rendering to DOM
+        return messageOutput
+    };
+
+    return null
 }
 
 export default MessageCompiler
